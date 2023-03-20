41dc89cc6befee4c7abddbe9086ba09dbe4fcd8c
CKZ <CKZ@DESKTOP-6G6PMSU>
2023-03-20 17:51:37 +0100

    DONE!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+    static public void greetings(Language arg1, Language ...args){
+        arg1.sayGreetings();
+        for (int i = 0; i<args.length; i++){
+            System.out.println(args[i].sayGreetings());
+
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Polish polski = new Polish();
+        English angielski = new English();
+        German niemiecki = new German();
+        Lolcat lolcat = new Lolcat();
+
+        greetings(polski, angielski, niemiecki, lolcat);
     }
 }
